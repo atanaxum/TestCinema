d215cd3bb73c7bf912655e5bcb17e5510888baa6
Atanaxum <nikita110598@mail.ru>
2018-03-15 19:34:37 +0100

    Avancement

--- app/src/main/java/com/example/autumn/testcinema/BaseData.java
+++ app/src/main/java/com/example/autumn/testcinema/BaseData.java
@@ -1,8 +1,12 @@
 package com.example.autumn.testcinema;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Autumn on 14/03/2018.
@@ -10,32 +14,102 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BaseData extends SQLiteOpenHelper {
 
-    private static final String TABLE = "questions";
-    private static final String COL_ID = "ID";
-    private static final String COL_REP = "REP";
-    private static final String COL_REP_USER = "REP";
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "questions";
+    public static final String COL_ID = "ID";
+    public static final String COL_REP = "REP";
+    public static final String COL_USER = "USR";
+    private static final String TAG = "BaseData" ;
 
-    private static final String CREATE_BDD = "CREATE TABLE " + TABLE + " ("
-            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COL_REP + " TEXT NOT NULL, "
-            + COL_REP_USER + " TEXT NOT NULL);";
-
-    public BaseData(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public BaseData(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        Log.d( TAG,"BD Constructor Finish" );
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //on crée la table à partir de la requête écrite dans la variable CREATE_BDD
-        db.execSQL(CREATE_BDD);
+
+
+        this.supprimer( db );
+        Log.d( TAG,"BD CREATE Start" );
+        String s = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
+                + COL_ID + " INTEGER PRIMARY KEY, "
+                + COL_REP + " INTEGER NOT NULL, "
+                + COL_USER + " INTEGER);";
+        db.execSQL(s);
+        Log.d( TAG,"BD CREATE Finish" );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //On peut faire ce qu'on veut ici moi j'ai décidé de supprimer la table et de la recréer
-        //comme ça lorsque je change la version les id repartent de 0
-        db.execSQL("DROP TABLE " + TABLE + ";");
+        this.supprimer( db );
         onCreate(db);
     }
 
+    public void supprimer(SQLiteDatabase db){
+        String s = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+        db.execSQL(s);
+    }
+
+    public boolean insertData(int id, int rep,int usr) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ID,id);
+        contentValues.put(COL_REP,rep);
+        contentValues.put(COL_USER,usr);
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        db.close();
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public int getReponse(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Log.d(TAG, "Jusque la ça marche old");
+
+        Cursor cursor = db.query(TABLE_NAME,
+                new String[]{COL_ID,COL_REP},
+                COL_ID + "=?",
+                new String[]{String.valueOf(id)},
+                null, null, null, null);
+        Log.d(TAG, "REP ap cursor");
+
+        if (cursor != null) cursor.moveToFirst();
+
+        int reponse = Integer.parseInt(cursor.getString(1));
+        Log.d(TAG, "REP avant return");
+        cursor.close();
+
+        return reponse;
+    }
+
+    public int getMax() {
+        Log.d(TAG,"MAX");
+        SQLiteDatabase db = this.getWritableDatabase();
+        String s = "SELECT count(ID) as nb FROM "+TABLE_NAME;
+        Cursor cursor = db.rawQuery(s,null);
+        cursor.moveToFirst();
+        Log.d(TAG,"MAX = "+cursor.getString( cursor.getColumnIndex( "nb" )));
+        if(0 != Integer.valueOf( cursor.getString( cursor.getColumnIndex( "nb" )) )) {
+            String z = "SELECT max(ID) as Max FROM "+TABLE_NAME;
+            Cursor zcursor = db.rawQuery(z,null);
+            zcursor.moveToFirst();
+            Log.d(TAG,"MAX = "+cursor.getString( zcursor.getColumnIndex( "Max" )));
+            return Integer.valueOf( cursor.getString( zcursor.getColumnIndex( "Max" )) ) ;
+        }
+        return 0;
+    }
+
+    public boolean updateData(String id,int rep,int usr) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ID,id);
+        contentValues.put(COL_REP,rep);
+        contentValues.put(COL_USER,usr);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        db.close();
+        return true;
+    }
 }
--- app/src/main/java/com/example/autumn/testcinema/Quizz.java
+++ app/src/main/java/com/example/autumn/testcinema/Quizz.java
@@ -6,24 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Quizz extends AppCompatActivity {
     private static final String TAG = "Quizz";
     private SectionsPageAdapter sectionsPageAdapter;
     private ViewPager viewPager;
-    private BaseDataQuestion bdq;
+    private BaseData bdq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
-        //Log.d(TAG, "onCreate: Lancement");
-        //COMMENT
+        Log.d( TAG,"Allumé" );
 
-        this.initialiserBD();
+
+        bdq = new BaseData( this );
+        this.inintBDQ();
+        Log.d( TAG,"BD Crée" );
 
         sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
-
         viewPager = findViewById(R.id.container);
         afficherFragment(viewPager);
 
@@ -32,28 +34,48 @@ public class Quizz extends AppCompatActivity {
 
     }
 
-    private void initialiserBD(){
-        bdq = new BaseDataQuestion(this);
-        bdq.open();
-        bdq.insertQuestion(1,2 );
-        bdq.insertQuestion( 2,3 );
-        bdq.close();
+    public BaseData getBDQ(){
+        return bdq;
     }
+
+    public void inintBDQ(){
+        if(this.getBDQ().getMax()==0){
+            this.addData( 1,3,9 );
+            this.addData( 2,1,9 );
+            this.addData( 3,2,9 );
+            this.addData( 4,2,9 );
+            this.addData( 5,2,9 );
+        }else{
+            Toast.makeText(this,"BD exist",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     private void afficherFragment(ViewPager viewPager){
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-        adapter.ajouterFragment(new QuizzFragment(1),"Question 1");
-        adapter.ajouterFragment(new QuizzFragment(2),"Question 2");
+        adapter.ajouterFragment(new QuizzFragment(1),"Question "+1);
+        adapter.ajouterFragment(new QuizzFragment(2),"Question "+2);
+        adapter.ajouterFragment(new QuizzFragment(3),"Question "+3);
+        adapter.ajouterFragment(new QuizzFragment(4),"Question "+4);
+        adapter.ajouterFragment(new QuizzFragment(5),"Question "+5);
         viewPager.setAdapter(adapter);
 
     }
 
 
+    public void updateData(int id, int rep) {
+        boolean isUpdate = bdq.updateData(String.valueOf( id ), bdq.getReponse( id ), rep);
+        if(isUpdate == true)
+            Toast.makeText(this,"Data Update",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this,"Data not Updated",Toast.LENGTH_SHORT).show();
+    }
 
-
-
-
-
-
-
+    public  void addData(int id, int rep, int usr) {
+        boolean isInserted = bdq.insertData(id, rep, usr);
+        if(isInserted == true)
+            Toast.makeText(this,String.valueOf( this.getBDQ().getMax() ),Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this,"Data not Inserted",Toast.LENGTH_SHORT).show();
+    }
 }
