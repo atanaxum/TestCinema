ac8ef84e876f172086dea4eac6b5f4e17a998020
Atanaxum <nikita110598@mail.ru>
2018-03-16 21:47:23 +0100

    Landscape Controle du dernier bouton pour que toutes les reponses soient données Son du bouton de validation Ajout de texte Passage en FullSCreen

--- app/src/main/java/com/example/autumn/testcinema/BaseData.java
+++ app/src/main/java/com/example/autumn/testcinema/BaseData.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by Autumn on 14/03/2018.
@@ -14,11 +13,11 @@ import android.widget.Toast;
 
 public class BaseData extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Student.db";
-    public static final String TABLE_NAME = "questions";
-    public static final String COL_ID = "ID";
-    public static final String COL_REP = "REP";
-    public static final String COL_USER = "USR";
+    public static final String DATABASE_NAME = "questionnaire.db";          //Nom du fichier contenant la bd sur le smartphone
+    public static final String TABLE_NAME = "questions";                    //Nom de la table BD
+    public static final String COL_ID = "ID";                               //Nom de la colonne contenant les ids des questions
+    public static final String COL_REP = "REP";                             //Nom de la colonne contenant les numeros des reponses justes
+    public static final String COL_USER = "USR";                            //Nom de la colonne contenant les numeros des reponses données par l'utilisateur
     private static final String TAG = "BaseData" ;
 
     public BaseData(Context context) {
@@ -28,81 +27,63 @@ public class BaseData extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-
-        this.supprimer( db );
         Log.d( TAG,"BD CREATE Start" );
+
         String s = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
                 + COL_ID + " INTEGER PRIMARY KEY, "
                 + COL_REP + " INTEGER NOT NULL, "
                 + COL_USER + " INTEGER);";
         db.execSQL(s);
+
         Log.d( TAG,"BD CREATE Finish" );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        this.supprimer( db );
-        onCreate(db);
-    }
-
-    public void supprimer(SQLiteDatabase db){
+        Log.d( TAG,"BD UPGRADE Start" );
         String s = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
         db.execSQL(s);
+        Log.d( TAG,"BD DELETE Finish" );
+
+        onCreate(db);
+        Log.d( TAG,"BD UPGRADE Finish" );
     }
 
+    /**
+     * Inserer une nouvelle valeur dans la BD
+     * @param id est l'id de la question à inserer
+     * @param rep est la reponse juste à la question
+     * @param usr est la reponse donnée par l'utilisateur
+     * @return VRAI si l'Insertion est un succes
+     */
     public boolean insertData(int id, int rep,int usr) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
+        Log.d( TAG,"BD INSERT Start" );
+        SQLiteDatabase db = this.getWritableDatabase();                     //Ecrire dans la BD
+        ContentValues contentValues = new ContentValues();                  //Variables à inserer dans la teble
         contentValues.put(COL_ID,id);
         contentValues.put(COL_REP,rep);
         contentValues.put(COL_USER,usr);
         long result = db.insert(TABLE_NAME,null ,contentValues);
-        db.close();
-        if(result == -1)
+        db.close();                                                         //Toujours fermer la BD apres modification
+        if(result == -1){
+            Log.d( TAG,"BD INSERT Fail" );
             return false;
-        else
+
+        }else{
+            Log.d( TAG,"BD INSERT Succes" );
             return true;
-    }
-
-    public int getReponse(int id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Log.d(TAG, "Jusque la ça marche old");
-
-        Cursor cursor = db.query(TABLE_NAME,
-                new String[]{COL_ID,COL_REP},
-                COL_ID + "=?",
-                new String[]{String.valueOf(id)},
-                null, null, null, null);
-        Log.d(TAG, "REP ap cursor");
-
-        if (cursor != null) cursor.moveToFirst();
-
-        int reponse = Integer.parseInt(cursor.getString(1));
-        Log.d(TAG, "REP avant return");
-        cursor.close();
-
-        return reponse;
-    }
-
-    public int getMax() {
-        Log.d(TAG,"MAX");
-        SQLiteDatabase db = this.getWritableDatabase();
-        String s = "SELECT count(ID) as nb FROM "+TABLE_NAME;
-        Cursor cursor = db.rawQuery(s,null);
-        cursor.moveToFirst();
-        Log.d(TAG,"MAX = "+cursor.getString( cursor.getColumnIndex( "nb" )));
-        if(0 != Integer.valueOf( cursor.getString( cursor.getColumnIndex( "nb" )) )) {
-            String z = "SELECT max(ID) as Max FROM "+TABLE_NAME;
-            Cursor zcursor = db.rawQuery(z,null);
-            zcursor.moveToFirst();
-            Log.d(TAG,"MAX = "+cursor.getString( zcursor.getColumnIndex( "Max" )));
-            return Integer.valueOf( cursor.getString( zcursor.getColumnIndex( "Max" )) ) ;
         }
-        return 0;
     }
 
+    /**
+     * Mettre à Jour la BD
+     * @param id est l'id de la question à mettre à jour
+     * @param rep est la reponse juste à la question
+     * @param usr est la reponse donnée par l'utilisateur
+     * @return VRAI si la Mise à Jour est un succes
+     */
     public boolean updateData(String id,int rep,int usr) {
+        Log.d(TAG,"BD UPDATE "+id+" Start avec usr = "+usr);
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_ID,id);
@@ -110,6 +91,72 @@ public class BaseData extends SQLiteOpenHelper {
         contentValues.put(COL_USER,usr);
         db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
         db.close();
+        Log.d(TAG,"BD UPDATE Finish");
         return true;
+    }
+
+    /**
+     * Renvoie la reponse juste à une question donnée
+     * @param id est l'ID de la question
+     * @return Numero correspondant à la bonne reponse
+     */
+    public int getReponse(int id) {
+        Log.d(TAG, "GET REPONSE Start");
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME,
+                new String[]{COL_ID,COL_REP},
+                COL_ID + "=?",
+                new String[]{String.valueOf(id)},
+                null, null, null, null);    //Requete recuperant la reponse juste à une question
+        Log.d(TAG, "GET REPONSE Cursor créé avec succes");
+
+        if (cursor != null) cursor.moveToFirst();
+
+        int reponse = Integer.parseInt(cursor.getString(1));
+        cursor.close();
+
+        return reponse;
+    }
+
+    /**
+     * Renvoie le nombre de questions enregistrés dans la BD
+     * @return ID maximum enregistré
+     */
+    public int getMax() {
+        Log.d(TAG,"MAX Start");
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String s = "SELECT count("+COL_ID+") as nb FROM "+TABLE_NAME;
+        Cursor cursor = db.rawQuery(s,null);
+        cursor.moveToFirst();
+
+        Log.d(TAG,"MAX count(ID) = "+cursor.getString( cursor.getColumnIndex( "nb" )));
+
+        //Verifie que la table n'est pas vide pour renvoyer un ID maximum (ce qui correspond au nombre maximum de questions
+        if(0 != Integer.valueOf( cursor.getString( cursor.getColumnIndex( "nb" )) )) {
+            String z = "SELECT max("+COL_ID+") as Max FROM "+TABLE_NAME;
+            Cursor zcursor = db.rawQuery(z,null);
+            zcursor.moveToFirst();
+            Log.d(TAG,"MAX max(ID) = "+cursor.getString( zcursor.getColumnIndex( "Max" )));
+            return Integer.valueOf( cursor.getString( zcursor.getColumnIndex( "Max" )) ) ;
+        }
+        return 0;
+    }
+
+    /**
+     * Renvoie le nombre de reponses d'utilisateur enregistrés dans la BD
+     * @return COL_USER maximum enregistré
+     */
+    public int getNbReponse() {
+        Log.d(TAG,"NB REPONSE Start");
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String s = "SELECT count("+COL_USER+") as nb FROM "+TABLE_NAME + " WHERE "+COL_USER+" <> 9";
+        Cursor cursor = db.rawQuery(s,null);
+        cursor.moveToFirst();
+
+        Log.d(TAG,"NB REPONSE count(COL_USR) = "+cursor.getString( cursor.getColumnIndex( "nb" )));
+        return Integer.valueOf( cursor.getString( cursor.getColumnIndex( "nb" ))) ;
     }
 }
--- app/src/main/java/com/example/autumn/testcinema/QuizzFragment.java
+++ app/src/main/java/com/example/autumn/testcinema/QuizzFragment.java
@@ -1,10 +1,9 @@
 package com.example.autumn.testcinema;
 
 import android.annotation.SuppressLint;
-import android.os.Build;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -31,31 +31,53 @@ public class QuizzFragment extends Fragment{
 
     ViewPager page;
 
-    public QuizzFragment(int id ) {
+    public QuizzFragment(int id) {
+        super();
         this.id = id;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        setRetainInstance( true );                                      //Necessaire pour faire fonctionner l'activité en landscape
         View v =inflater.inflate(R.layout.quiz_question,container,false);
         page = getActivity().findViewById(R.id.container);
+        this.setUSR( 8 );                                               //Une reponse utilisateur fixée a 9 (car+1 dans le setUSR()) correspond à "l'utilisateur n'a pas repondu
 
-        int resQuest = getResources().getIdentifier("question"+ this.getID(),"string", v.getContext().getPackageName());
-        TextView quest = v.findViewById(R.id.question);
+        int resQuest = getResources().getIdentifier(
+                "question"+ this.getID(),
+                "string",
+                v.getContext().getPackageName());                       //Recuperation de la question dans les Ressources
+        TextView quest = v.findViewById(R.id.question);                 //Création du champ Text contenant la question
         quest.setText(getString( resQuest ));
 
+        //RadioGroupe contenant les reponses possibles
         final RadioGroup rg = v.findViewById( R.id.rg );
-        this.setUSR( rg.indexOfChild( v.findViewById( rg.getCheckedRadioButtonId())) );
+        //Creation du son d'un boutton
+        final MediaPlayer sound_button = MediaPlayer.create(v.getContext(), R.raw.button );
+
+        //this.setUSR( rg.indexOfChild( v.findViewById( rg.getCheckedRadioButtonId())) );
 
         btnValider = v.findViewById(R.id.valider);
         btnValider.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG,"QUESTION REPONDUE = "+QuizzFragment.this.getID());
+                sound_button.start();
                 int id = QuizzFragment.this.getID();
                 int usr = QuizzFragment.this.getUSR();
-                page.setCurrentItem(id);
                 ((Quizz)getActivity()).updateData( id,usr );
+                if(QuizzFragment.this.getID() == 10){
+                    Log.d(TAG,"DERNIERE QUESTION repondue");
+                    if( ((Quizz)getActivity()).getBDQ().getNbReponse() != 10 ){
+                        Log.d(TAG,"NB QUESTIONS REPONDUES != NB REPONSES DONNEES");
+                        Toast.makeText(QuizzFragment.this.getContext(),"Vous n'avez pas repondu à toutes les questions",Toast.LENGTH_LONG).show();
+                    }else{
+                        //TRAITEMENT
+                    }
+                }else{
+                    page.setCurrentItem(id);                        //On passe à la question suivante
+                }
             }
         } );
         btnValider.setEnabled( false );
@@ -68,28 +90,41 @@ public class QuizzFragment extends Fragment{
             btnRep.setOnClickListener( new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
                     int id = QuizzFragment.this.getID();
                     QuizzFragment.this.setUSR( rg.indexOfChild( view.findViewById( rg.getCheckedRadioButtonId())) );
                     int usr = QuizzFragment.this.getUSR();
                     int rep = ((Quizz)getActivity()).getBDQ().getReponse( id );
                     ((Quizz)getActivity()).getBDQ().updateData( String.valueOf( id ), rep, usr );
-                    Toast.makeText(getActivity().getBaseContext(),String.valueOf( rep ),Toast.LENGTH_SHORT).show();
                     btnValider.setEnabled( true );
-
                 }
             } );
         }
-
         return v;
     }
 
+    /**
+     * Renvoye l'id de la question actuelle
+     * @return l'ID
+     */
     public int getID(){
         return this.id;
     }
+
+    /**
+     * Renvoye la reponse donnée par l'utilisateur
+     * @return le numero correspondant à la reponse de l'utilisateur
+     */
     public int getUSR(){
         return this.usr;
     }
-    public void setUSR(int usr){ this.usr = usr +1;}
+
+    /**
+     * Enregistre la reponse donnée par l'utilisateur
+     * @param usr le numero correspondant à la reponse de l'utilisateur
+     */
+    public void setUSR(int usr){
+        //+1 necessaire car les numeros des radioButton = 0,1,2 alors que numero des Reponses dans la BD = 1,2,3
+        this.usr = usr +1;
+    }
 
 }
